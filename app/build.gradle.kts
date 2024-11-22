2931cdc31af00d30304425f737800a9d7d7260f9
Nurdin <mrsunnysummer@gmail.com>
2024-11-23 04:23:23 +0700

    chore: upgrade minimum and target sdk from 34 to 35

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.nudriin.mymediaplayer"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.nudriin.mymediaplayer"
         minSdk = 24
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
